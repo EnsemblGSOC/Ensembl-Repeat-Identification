4766eb7edcdeb150f7ca677138c583e4078b4ab2
yangtcai <yangtcai@gmail.com>
2022-05-13 20:57:08 +0800

    get_all

--- anno.py
+++ anno.py
@@ -2,11 +2,11 @@
 Author: yangtcai yangtcai@gmail.com
 Date: 2022-05-12 22:31:30
 LastEditors: yangtcai yangtcai@gmail.com
-LastEditTime: 2022-05-12 22:40:29
+LastEditTime: 2022-05-13 20:53:11
 FilePath: /undefined/Users/caiyz/Desktop/anno
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
-from __future__ import annotations
+
 import requests, json
 import csv 
 from tqdm import tqdm
@@ -27,16 +27,24 @@ def get_annotation( sp, chr, op, ed):
 
     response = requests.get(url, params=params)
     results = response.json()
-    anno = []
-    for hit in tqdm(results['hits']):
+    annotations = []
+    for hit in results['hits']:
         if hit['type'] == 'LTR':
-            anno.append([hit['ali_start'], hit['ali_end'], get_subtype(hit['accession'])])
+            annotations.append([hit['ali_start'], hit['ali_end'], get_subtype(hit['accession'])])
 
-    save_to_csv(anno)
+    save_to_csv(annotations)
 
-def save_to_csv(anno):
-    with open("test.csv","w+") as csvfile: 
+def save_to_csv(annotations):
+    with open("test.csv","a+") as csvfile: 
         writer = csv.writer(csvfile)
-        writer.writerows(anno)
+        writer.writerows(annotations)
 
-get_annotation('hg38', 'chr3', 147733000, 147766820)
+#get_annotation('hg38', 'chr3', 147733000, 147766820)
+
+def get_all():
+    for i in tqdm(range(0, 198295559, 100000)):
+        get_annotation('hg38', 'chr3', i, i+100000)
+
+
+
+get_all()
