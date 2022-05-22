62f084b2c7ae32739677bf7596a557c0abe6de1c
yangtcai <yangtcai@gmail.com>
2022-05-22 20:57:46 +0800

    remove header

--- anno.py
+++ anno.py
@@ -1,11 +1,3 @@
-"""
-Author: yangtcai yangtcai@gmail.com
-Date: 2022-05-12 22:31:30
-LastEditors: yangtcai yangtcai@gmail.com
-LastEditTime: 2022-05-16 17:53:03
-FilePath: /undefined/Users/caiyz/Desktop/anno
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-"""
 
 
 # standard library
