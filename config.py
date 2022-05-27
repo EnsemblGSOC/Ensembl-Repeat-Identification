1c8108ef9a0674e3a8a665754de1cefbb0696a25
yangtcai <yangtcai@gmail.com>
2022-05-27 21:06:01 +0800

    update the generation of reference datasets

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
+'''
+Author: yangtcai yangtcai@gmail.com
+Date: 2022-05-22 18:34:12
+LastEditors: yangtcai yangtcai@gmail.com
+LastEditTime: 2022-05-27 15:01:52
+FilePath: /Ensembl-Repeat-Identification/chr_info.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 chr_length = {
     "chr1": 248956422,
     "chr10": 133797422,
@@ -23,4 +31,8 @@ chr_length = {
     "chr9": 138394717,
     "chrX": 156040895,
     "chrY": 57227415,
+}
+url_species = {
+    'hg38':'https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz',
+    'mm10':'http://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/chromFa.tar.gz'
 }
