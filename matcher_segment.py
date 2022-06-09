f7ad9e27443db475452a3fd87dd72d7384f461d7
yangtcai <yangtcai@gmail.com>
2022-06-09 17:03:42 +0800

    add matcher original source

--- matcher_segment.py
+++ matcher_segment.py
@@ -1,5 +1,8 @@
 """
 Modules to compute the matching cost and solve the corresponding LSAP.
+
+Copy-paste from the following link with modifications.
+Link: https://github.com/facebookresearch/detr/blob/main/models/matcher.py#L12
 """
 import torch
 from scipy.optimize import linear_sum_assignment
@@ -17,13 +20,6 @@ class HungarianMatcher(nn.Module):
     def __init__(
         self, cost_class: float = 1, cost_segments: float = 1, cost_siou: float = 1
     ):
-        """Creates the matcher
-
-        Params:
-            cost_class: This is the relative weight of the classification error in the matching cost
-            cost_segments: This is the relative weight of the L1 error of the bounding box coordinates in the matching cost
-            cost_siou: This is the relative weight of the giou loss of the bounding box in the matching cost
-        """
         super().__init__()
         self.cost_class = cost_class
         self.cost_segments = cost_segments
@@ -34,32 +30,14 @@ class HungarianMatcher(nn.Module):
 
     @torch.no_grad()
     def forward(self, outputs, targets):
-        """Performs the matching
 
-        Params:
-            outputs: This is a dict that contains at least these entries:
-                 "pred_logits": Tensor of dim [batch_size, num_queries, num_classes] with the classification logits
-                 "pred_boxes": Tensor of dim [batch_size, num_queries, 4] with the predicted box coordinates
-
-            targets: This is a list of targets (len(targets) = batch_size), where each target is a dict containing:
-                 "labels": Tensor of dim [num_target_boxes] (where num_target_boxes is the number of ground-truth
-                           objects in the target) containing the class labels
-                 "boxes": Tensor of dim [num_target_boxes, 4] containing the target box coordinates
-
-        Returns:
-            A list of size batch_size, containing tuples of (index_i, index_j) where:
-                - index_i is the indices of the selected predictions (in order)
-                - index_j is the indices of the corresponding selected targets (in order)
-            For each batch element, it holds:
-                len(index_i) = len(index_j) = min(num_queries, num_target_boxes)
-        """
         bs, num_queries = outputs["pred_logits"].shape[:2]
 
         # We flatten to compute the cost matrices in a batch
         out_prob = (
             outputs["pred_logits"].flatten(0, 1).softmax(-1)
         )  # [batch_size * num_queries, num_classes]
-        out_segments = outputs["pred_boxes"].flatten(
+        out_segments = outputs["pred_boundaries"].flatten(
             0, 1
         )  # [batch_size * num_queries, 2]
 
@@ -129,7 +107,7 @@ if __name__ == "__main__":
     pred_logits = torch.rand(batch_size, num_queries, num_classes)
     pred_boxes = torch.rand(batch_size, num_queries, 2)
 
-    outputs = {"pred_logits": pred_logits, "pred_boxes": pred_boxes}
+    outputs = {"pred_logits": pred_logits, "pred_boundaries": pred_boxes}
 
     num_target_boxes = 8
 
