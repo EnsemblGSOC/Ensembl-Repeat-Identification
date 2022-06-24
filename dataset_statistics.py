23b50f83861396ab74fc4a996b47911ed04e0e3f
William Stark <william@ebi.ac.uk>
2022-06-24 11:35:27 +0100

    load repeat families to a pandas DataFrame, generate simple type and subtype frequency statistics

--- dataset_statistics.py
+++ dataset_statistics.py
@@ -14,10 +14,12 @@
 # ---
 
 # %%
+import json
 import pathlib
 
 # %%
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # %%
 # %load_ext autoreload
@@ -32,9 +34,14 @@ figsize = (16, 9)
 # %%
 data_directory = pathlib.Path("../data")
 
-annotations_path = data_directory / "annotations" / "hg38.hits"
+annotations_directory = data_directory / "annotations"
+
+# %% [markdown]
+# ## repeat annotations
 
 # %%
+annotations_path = annotations_directory / "hg38.hits"
+
 data = hits_to_dataframe(annotations_path)
 
 # %%
@@ -55,7 +62,7 @@ data.info()
 data.nunique()
 
 # %% [markdown]
-# ## column value counts
+# ### value counts
 
 # %%
 categorical_variables = {
@@ -74,7 +81,7 @@ for column in categorical_variables:
     plt.show()
 
 # %% [markdown] tags=[]
-# ## repeats length
+# ### repeats length
 
 # %%
 data["ali_length"] = abs(data["ali-en"] - data["ali-st"]) + 1
@@ -106,5 +113,105 @@ for count in range(1, 3 + 1):
     ax.axvline(x=x, color="red", linewidth=1)
 ax.set(xlabel="ali_length", ylabel="number of repeats")
 figure.add_axes(ax)
+
+# %%
+
+# %%
+
+# %%
+
+# %% [markdown]
+# ## repeat families
+
+# %%
+from pprint import pp as pprint
+
+
+repeat_families_path = annotations_directory / "repeat_families.json"
+
+# load repeat families
+with open(repeat_families_path) as json_file:
+    repeat_families = json.load(json_file)
+
+# %%
+counter = 0
+for accession, dictionary in repeat_families.items():
+    pprint(dictionary)
+    counter += 1
+    if counter == 3:
+        break
+    print()
+
+# %%
+
+# %%
+families = pd.DataFrame.from_records(
+    repeat_family for accession, repeat_family in repeat_families.items()
+)
+
+# %%
+families.head()
+
+# %%
+# show random sample of items
+num_items = 10
+random_seed = 5
+families.sample(num_items, random_state=random_seed).sort_index()
+
+# %%
+# concise summary of dataframe
+families.info()
+
+# %%
+families = families.drop("clades", axis=1)
+
+# %%
+# number of distinct elements
+families.nunique()
+
+# %% [markdown]
+# ### value counts
+
+# %%
+categorical_variables = {
+    "repeat_type_name",
+    "repeat_subtype_name",
+}
+
+# %%
+for column in categorical_variables:
+    column_value_counts = families[column].value_counts()
+    print(f"{column=}\n", column_value_counts, "\n")
+    column_value_counts[:30]
+    column_value_counts[:30].plot(kind="bar", figsize=figsize)
+    plt.show()
+
+# %%
+type_subtype_value_counts = families[
+    ["repeat_type_name", "repeat_subtype_name"]
+].value_counts()
+print(f"{column=}\n", type_subtype_value_counts, "\n")
+type_subtype_value_counts[:40]
+type_subtype_value_counts[:40].plot(kind="bar", figsize=figsize)
+plt.show()
+
+# %%
+
+# %%
+
+# %%
+
+# %% [markdown]
+# ## select a repeat type
+
+# %%
+repeat_type = "LTR"
+repeat_subset = families[families["repeat_type_name"] == repeat_type]
+
+# %%
+repeat_subset.sample(10, random_state=5).sort_index()
+
+# %%
+sorted(repeat_subset["classification"].unique())
 
 # %%
